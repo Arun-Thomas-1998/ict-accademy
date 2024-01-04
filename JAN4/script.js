755172de9be7f9bf6d42e6da4317c1803ddd2f16
Arun-Thomas-1998 <arun37579@gmail.com>
2024-01-04 08:05:07 +0530

    ict git introduction

---
+++ JAN4/script.js
@@ -0,0 +1,30 @@
+var arr=['a','b','c','d','e']
+// for(i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+//*** */ while 
+// i=0
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+//**do-while */
+let i=0
+do{
+    console.log(arr[i]);
+    i++;
+}while(i<arr.length)
+
+
+
+// //**for of
+// for (const i of arr) {
+//     console.log(i);
+
+// }
+// //**for in
+// const person = { name: "John", age: 30, city: "New York" };
+
+// for (const key in person) {
+//   console.log(key + ": " + person[key]);
+// }
